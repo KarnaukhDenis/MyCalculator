cea0d6a4cf29c9074017ec5683723288b9633f61
Karnaukh Denis <LV426D@mail.ru>
2017-10-04 22:50:33 +1000

    update id button

--- app/src/main/java/com/example/denis/mycalculator/CalcActivity.java
+++ app/src/main/java/com/example/denis/mycalculator/CalcActivity.java
@@ -3,31 +3,38 @@ package com.example.denis.mycalculator;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class CalcActivity extends MainActivity {
 
-    @BindView(R2.id.text) TextView text;
-    @BindView(R2.id.button) Button button;
-    @BindView(R2.id.button1) Button button1;
-    @BindView(R2.id.button2) Button button2;
-    @BindView(R2.id.button3) Button button3;
-    @BindView(R2.id.button4) Button button4;
-    @BindView(R2.id.button5) Button button5;
-    @BindView(R2.id.button6) Button button6;
-    @BindView(R2.id.button7) Button button7;
-    @BindView(R2.id.button8) Button button8;
-    @BindView(R2.id.button9) Button button9;
-    @BindView(R2.id.button10) Button button10;
-    @BindView(R2.id.button11) Button button11;
-    @BindView(R2.id.buttonPlus) Button buttonPlus;
-    @BindView(R2.id.buttonMin) Button buttonMin;
-    @BindView(R2.id.buttonDelenie) Button buttonDelenie;
+    @BindView(R2.id.editText) EditText editText;
+    @BindView(R2.id.buttonOne) Button buttonOne;
+    @BindView(R2.id.buttonTwo) Button buttonTwo;
+    @BindView(R2.id.buttonThree) Button buttonThree;
+    @BindView(R2.id.buttonFour) Button buttonFour;
+    @BindView(R2.id.buttonFive) Button buttonFive;
+    @BindView(R2.id.buttonSix) Button buttonSix;
+    @BindView(R2.id.buttonSeven) Button buttonSeven;
+    @BindView(R2.id.buttonEight) Button buttonEight;
+    @BindView(R2.id.buttonNine) Button buttonNine;
+    @BindView(R2.id.buttonZero) Button buttonZero;
+    @BindView(R2.id.buttonMul) Button buttonMul;
     @BindView(R2.id.buttonDot) Button buttonDot;
-    @BindView(R2.id.buttonEqual1) Button buttonEqual1;
+    @BindView(R2.id.buttonPlus) Button buttonPlus;
+    @BindView(R2.id.buttonMinus) Button buttonMin;
+    @BindView(R2.id.buttonDelenie) Button buttonDelenie;
+    @BindView(R2.id.buttonC) Button buttonC;
+    @BindView(R2.id.buttonEqual) Button buttonEqual;
+
+    enum MathOperation {PLUS, MINUS, DIVIDE, MULTIPLY}
+    MathOperation mathOperation;
+    Float valueOne;
+    Float valueSecond;
+    Float[]  resultEdit;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,62 +46,79 @@ public class CalcActivity extends MainActivity {
             @Override
             public void onClick(View v) {
                 switch (v.getId()) {
-                    case R2.id.text:
+                    case R2.id.editText:
 
                         break;
-                    case R2.id.button:
-
+                    case R2.id.buttonOne:
+                        editText.setText("1");
+                        editText.setText(editText.getText()+"1");
                         break;
-                    case R2.id.button1:
-                        //Ваш код для кнопки "1"
+                    case R2.id.buttonTwo:
+                        editText.setText("2");
                         break;
-                    case R2.id.button2:
-                        //Ваш код для кнопки "1"
+                    case R2.id.buttonThree:
+                        editText.setText("3");
                         break;
-                    case R2.id.button3:
-                        //Ваш код для кнопки "1"
+                    case R2.id.buttonFour:
+                        editText.setText("4");
                         break;
-                    case R2.id.button4:
-                        //Ваш код для кнопки "1"
+                    case R2.id.buttonFive:
+                        editText.setText("5");
                         break;
-                    case R2.id.button5:
-                        //Ваш код для кнопки "1"
+                    case R2.id.buttonSix:
+                        editText.setText("6");
                         break;
-                    case R2.id.button6:
-                        //Ваш код для кнопки "1"
+                    case R2.id.buttonSeven:
+                        editText.setText("7");
                         break;
-                    case R2.id.button7:
-                        //Ваш код для кнопки "1"
+                    case R2.id.buttonEight:
+                        editText.setText("8");
                         break;
-                    case R2.id.button8:
-                        //Ваш код для кнопки "1"
+                    case R2.id.buttonNine:
+                        editText.setText("9");
                         break;
-                    case R2.id.button9:
-                        //Ваш код для кнопки "1"
+                    case R2.id.buttonZero:
+                        editText.setText("0");
                         break;
-                    case R2.id.button10:
-                        //Ваш код для кнопки "1"
-                        break;
-                    case R2.id.button11:
-                        //Ваш код для кнопки "1"
-                        break;
-                    case R2.id.buttonDelenie:
-                        //Ваш код для кнопки "1"
-                        break;
-                    case R2.id.buttonPlus:
-                        //Ваш код для кнопки "1"
-                        break;
-                    case R2.id.buttonMin:
-                        //Ваш код для кнопки "1"
-                        break;
-                    case R2.id.buttonEqual1:
-                        //Ваш код для кнопки "1"
+                    case R2.id.buttonMul:
+                        //Ваш код для кнопки "*"
                         break;
                     case R2.id.buttonDot:
-                        //Ваш код для кнопки "1"
+                        editText.setText(".");
+                        break;
+                    case R2.id.buttonDelenie:
+                        //Ваш код для кнопки "/"
+                        break;
+                    case R2.id.buttonPlus:
+
+                        break;
+                    case R2.id.buttonMinus:
+                        //Ваш код для кнопки "-"
+                        break;
+                    case R2.id.buttonEqual:
+                        //Ваш код для кнопки "="
+                        break;
+                    case R2.id.buttonC:
+                        //Ваш код для кнопки "C"
                         break;
                 }
             }
         };
+        buttonOne.setOnClickListener(globalClickListener);
+        buttonTwo.setOnClickListener(globalClickListener);
+        buttonThree.setOnClickListener(globalClickListener);
+        buttonFour.setOnClickListener(globalClickListener);
+        buttonFive.setOnClickListener(globalClickListener);
+        buttonSix.setOnClickListener(globalClickListener);
+        buttonSeven.setOnClickListener(globalClickListener);
+        buttonEight.setOnClickListener(globalClickListener);
+        buttonNine.setOnClickListener(globalClickListener);
+        buttonDelenie.setOnClickListener(globalClickListener);
+        buttonDot.setOnClickListener(globalClickListener);
+        buttonPlus.setOnClickListener(globalClickListener);
+        buttonMin.setOnClickListener(globalClickListener);
+        buttonC.setOnClickListener(globalClickListener);
+        buttonEqual.setOnClickListener(globalClickListener);
+        buttonMul.setOnClickListener(globalClickListener);
     }
 }
